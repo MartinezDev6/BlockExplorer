31859ee5ecdbfa9d7d76db5cd3e9330ea14650d9
MartinezDev6 <comqkblkbu@hotmail.com>
2025-01-02 15:27:43 +0800

    initial setup, basic react structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Block Explorer</h1>
+        <p>Ethereum Blockchain Explorer</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
